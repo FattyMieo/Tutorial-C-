84e2da7b4e1bb62196501d9ccef53af0df79f949
FattyMieo <fong2009@live.com.my>
2017-05-14 23:06:02 +0800

    Create function "MessageSelector"

--- main.cpp
+++ main.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
+void MessageSelector()
+{
+	int i = -1;
+	bool canExit = false;
+	
+	do
+	{
+		cout << "Type an integer equal or bigger than 0: ";
+		cin >> i;
+		
+		if(!cin.good())
+		{
+			cin.clear();
+			cin.ignore(1000, '\n');
+			canExit = false;
+		}
+		else
+		{
+			canExit = i >= 0;
+		}
+	}
+	while(!canExit);
+	
+	switch(i)
+	{
+		case 0:
+			cout << "Hello World" << endl;
+			break;
+		case 1:
+			cout << "Hello FunBun" << endl;
+			break;
+		case 2:
+			cout << "Hello GPS" << endl;
+			break;
+		default:
+			cout << "Hello Everyone" << endl;
+			break;
+	}
+}
+
 int main()
 {
-	cout << "Hello World" << endl;
+	//cout << "Hello World" << endl;
+	MessageSelector();
 	
 	system("PAUSE");
 	return 0;
